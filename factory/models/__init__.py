a88d3ce5377d57d1a4f14edabb8272e55bc9497d
Olga-Yar <120413444+Olga-Yar@users.noreply.github.com>
2023-10-06 10:58:35 +0000

    создание структуры проекта и настройка

---
+++ factory/models/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'Company',
+    'Contacts',
+    'Factory',
+    'Products',
+    'Retail'
+]
+
+from factory.models.company import Company
+from factory.models.contacts import Contacts
+from factory.models.factory import Factory
+from factory.models.products import Products
+from factory.models.retail import Retail
